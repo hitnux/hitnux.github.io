445ea5d795855151389e407140df90e0d2455d8c
hlilbilgin <hlilbilgin@gmail.com>
2020-03-08 15:40:03 +0300

    up

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+    $.getJSON('https://api.github.com/users/hitnux',{},function(data){
+        main(data);
+    });
+});
+function main(data){
+    Vue.component('navbar', {
+        template: '#navbar'
+      });
+      Vue.component('mycard', {
+        template: '#mycard',
+        props: ['title','button','href','user']
+      });
+      Vue.component('myinfo', {
+        template: '#myinfo'
+      });
+    new Vue({
+        el: '#app',
+        data: {
+            message: data,
+            repos: data["public_repos"],
+            followers: data["followers"],
+            following: data["following"],
+            username: data["login"]
+        },
+        methods: {
+        }
+    });
+}
+function github(){
+    $.getJSON('https://api.github.com/users/hitnux',{},function(data){return data});
+}    
+    
